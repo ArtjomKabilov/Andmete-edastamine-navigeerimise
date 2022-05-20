a2a173e740d74d23cebf703f70e2bf0ba0937fb6
opilane <opilane@HP-CZC6297BB4>
2022-05-20 14:30:28 +0300

    Add project files.

---
+++ Andmete edastamine navigeerimise/Maakonnad.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Andmete_edastamine_navigeerimise
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Maakonnad : ContentPage
+    {
+        public Maakonnad()
+        {
+            InitializeComponent();
+        }
+    }
+}
